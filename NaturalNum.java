8d9b67b275b1099c2a5da3d030840b7cfc6837e2
neha-ahire <129039096+neha-ahire@users.noreply.github.com>
2023-03-27 15:27:44 +0530

    Add files via upload

---
+++ NaturalNum.java
@@ -0,0 +1,15 @@
+package copaAssingnments;
+// Write a program to calculate the sum of first 10 natural number.
+public class NaturalNum
+{
+  public static void main(String args[])
+   
+  {
+	  int i;
+	  for(i=1;i<=10;i++)
+	  {
+		  System.out.println("The natural number is "+i );
+	  }
+  }
+}
+
