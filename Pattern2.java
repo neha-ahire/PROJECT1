4421bb0753d90ed5bbc4a8a3511b1d648143a0cc
neha-ahire <129039096+neha-ahire@users.noreply.github.com>
2023-03-27 15:28:12 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,21 @@
+package copaAssingnments;
+
+public class Pattern2 
+{
+  public static void main(String args[])
+  {
+	  int i, j, n=6;   
+	 
+	for(i=0; i<n; i++)   
+	{   
+	 
+	for(j=0; j<=i; j++)   
+	{   
+	  
+	System.out.print("* ");   
+	}   
+
+	System.out.println();   
+	}   
+  }
+}
